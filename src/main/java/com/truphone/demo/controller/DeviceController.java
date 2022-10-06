cf8e9a052a0e7816e469fa0e7028c98ecfa4540e
Cláudio Gonçalves <claudiofilipesilvagoncalves@gmail.com>
2022-10-07 00:09:26 +0100

    Get device by identifier endpoint

--- src/main/java/com/truphone/demo/controller/DeviceController.java
+++ src/main/java/com/truphone/demo/controller/DeviceController.java
@@ -13,12 +13,14 @@ import com.truphone.demo.repository.DeviceRepository;
 import java.sql.Timestamp;
 import java.time.Clock;
 import java.time.Instant;
+import java.util.Optional;
 
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,7 +47,20 @@ public class DeviceController {
 	}
     }
     
-    @GetMapping("/test")
+
+    @GetMapping("/device/{id}")
+    public ResponseEntity<Device> getDeviceById(@PathVariable("id") long id) {
+        Optional<Device> deviceData = deviceRepository.findById(id);
+
+        if (deviceData.isPresent()) {
+            return new ResponseEntity<>(deviceData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @GetMapping("/device")
     public ResponseEntity<String> findByPublished() {
         return new ResponseEntity<>("Hello", HttpStatus.ACCEPTED);
     }
